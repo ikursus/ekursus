4b823f6ce4db6f1f604ec7ee10332c828c17ba4f
jomphp <gwbiz4u@gmail.com>
2017-02-15 12:24:11 +0800

    Update Form

--- resources/views/template_borang_permohonan_kursus.blade.php
+++ resources/views/template_borang_permohonan_kursus.blade.php
@@ -9,32 +9,37 @@
                 <div class="panel-heading">Borang Permohonan</div>
                 <div class="panel-body">
 
-<form method="POST">
-  
-{{ csrf_field() }}
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+{!! Form::open() !!}
 
   <div class="form-group">
-    <input type="text" name="nama_pemohon" placeholder="Isi nama anda..." class="form-control">
+    {!! Form::text('nama_pemohon', null, ['placeholder' => 'Isi nama anda...', 'class' => 'form-control']) !!}
   </div>
 
   <div class="form-group">
-    <input type="text" name="emel_pemohon" placeholder="Isi emel anda..." class="form-control">
+    {!! Form::email('emel_pemohon', null, ['placeholder' => 'Isi emel anda...', 'class' => 'form-control']) !!}
   </div>
 
   <div class="form-group">
-    <input type="text" name="telefon_pemohon" placeholder="Isi telefon anda..." class="form-control">
+    {!! Form::text('telefon_pemohon', null, ['placeholder' => 'Isi telefon anda...', 'class' => 'form-control']) !!}
   </div>
 
   <div class="form-group">
-    <select name="kursus_id" class="form-control">
-      <option value="1">Kursus PHP 15 Mac 2017</option>
-      <option value="2">Kursus Diving 20 Mac 2017</option>
-    </select>
+    {!! Form::select('kursus_id', ['1' => 'Kursus PHP 15 Mac 2017', '2' => 'Kursus Diving 20 Mac 2017'], null, ['placeholder' => 'Sila Pilih', 'class' => 'form-control']) !!}
   </div>
 
   <button class="btn btn-primary btn-block">Hantar Permohonan</button>
 
-</form>
+{!! Form::close() !!}
 
 </div>
 </div>
